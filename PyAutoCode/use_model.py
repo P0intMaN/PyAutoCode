57bb1c4b78ffa4f3bb03e66d21007284a4a44d22
Pratheek U <pratheekastro3@gmail.com>
2022-04-18 23:05:09 +0530

    Update use_model

--- PyAutoCode/use_model.py
+++ PyAutoCode/use_model.py
@@ -1,1 +1,25 @@
 # deploy this file in training computer. Configure later in huggingface.co
+# 
+#                                   OR
+# 
+# pip install transformers
+# 
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+#
+# tokenizer = AutoTokenizer.from_pretrained("P0intMaN/PyAutoCode")
+#
+# model = AutoModelForCausalLM.from_pretrained("P0intMaN/PyAutoCode")
+#
+# NOTE: input: single line or multi-line. Highly recommended to use doc-strings.
+# inp = """import pandas"""
+#
+# format_inp = inp.replace('\n', "<N>")
+# tokenize_inp = tokenizer.encode(format_inp, return_tensors='pt')
+# result = model.generate(tokenize_inp)
+#
+# decode_result = tokenizer.decode(result[0])
+# format_result = decode_result.replace('<N>', "\n")
+#
+# printing the result
+# print(format_result)
+# 
